879b157667d74878c33e69530bdce6144b5d140d
chu <chuanhu90@gmail.com>
2014-12-20 00:21:39 -0700

    save fb python file.  TODO now we can only get first 25 posts.

--- fb_get_posts.py
+++ fb_get_posts.py
@@ -5,39 +5,56 @@ Originally created by Mitchell Stewart.
 """
 import facebook
 import requests
-
+import json
 
 def some_action(post):
     """ Here you might want to do something with each post. E.g. grab the
     post's message (post['message']) or the post's picture (post['picture']).
     In this implementation we just print the post's created time.
     """
-    print post['created_time']
+    # print post['created_time']
+    print post
 
 
 # You'll need an access token here to do anything.  You can get a temporary one
 # here: https://developers.facebook.com/tools/explorer/
-access_token = 'CAACEdEose0cBABd1u4QTqnkPF3NU48nTupW7nHZC4mp9dmZCXTq73ZBfjmBI2PTOt6i3FgyGNk88Mqc6bWIstARgl8KCyDkZA6zJjZAUvz47vUNJIsWp3qgzZCJGVLdUAnPwrEZBg57KZAiwhxlZAlJ1zLZCTQ3j6fU5yZBmrYtgUTQqibN7vIHBrPoQMnWPwtTnifLxYPU5a9xZB6SGU1zop4n7'
+access_token = 'CAACEdEose0cBABCFf5vjPlWcaZA0XQyeOdVd0Rv5php7ANW1EF6huhrvnDECUoZCTpwubmKtKWmk71HecfJJCJZAz6MPfWWlIAhoEz6ZAf1OCzGO8IVRqZB4Wbz4jvzhFVpZCRcGvrZAfcCmbFhH6NlRDr76ZAQ7jE8GKzZAJRoBS4V9FsoVOjJZAW5C6HzyJ1VLba25knmZBO9Nm8m7IldMMbNJfdXGZCYCxIkZD'
 
-# Look at Bill Gates's profile for this example by using his Facebook id.
-user = 'BillGates'
+# users = ["comscoreinc", "conchoresources", "csodcommunity", "WorkAtBRAVO", "iHeartMedia", "CTGinc", "GrouponJobs", "InsperityJobs", "tmobilecareers", "ReachLocal", "SymmetryMedical", "Zillow"]
+users = ["ReachLocal", "SymmetryMedical", "Zillow"]
+# Look at user's profile for this example by using his Facebook id.
+for user in users:
 
-graph = facebook.GraphAPI(access_token)
-profile = graph.get_object(user)
-posts = graph.get_connections(profile['id'], 'posts')
+    graph = facebook.GraphAPI(access_token)
+    profile = graph.get_object(user)
+    print profile
+    # exit()
 
-# Wrap this block in a while loop so we can keep paginating requests until
-# finished.
-while True:
+    # user timeline
     try:
-        # Perform some action on each post in the collection we receive from
-        # Facebook.
-        [some_action(post=post) for post in posts['data']]
-        # Attempt to make a request to the next page of data, if it exists.
-        posts = requests.get(posts['paging']['next']).json()
-        print posts
-    except KeyError as e:
-        # When there are no more pages (['paging']['next']), break from the
-        # loop and end the script.
-        print "API error ", e.errno, e.strerror
-        break
+        posts = graph.get_connections(profile['id'], 'posts')
+    except:
+        pass
+
+    # job opening page posts.  TODO
+    # posts = graph.get_connections("118437573003", 'posts')
+    post_list = list()
+
+    # Wrap this block in a while loop so we can keep paginating requests until
+    # finished.
+    while True:
+        try:
+            # Perform some action on each post in the collection we receive from
+            # Facebook.
+            # [some_action(post=post) for post in posts['data']]
+            post_list.append(posts)
+            # Attempt to make a request to the next page of data, if it exists.
+            posts = requests.get(posts['paging']['next']).json()
+        except KeyError as e:
+            # When there are no more pages (['paging']['next']), break from the
+            # loop and end the script.
+            print "API error ", e
+            break
+
+    with open(user+"_timeline.json", "w") as outfile:
+        json.dump(post_list, outfile )
